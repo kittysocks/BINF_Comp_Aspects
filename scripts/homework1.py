678ef7bad54f841c3c84bc31cd7a6fa888f60dcb
Molly Miraglia <miragl82@students.rowan.edu>
2020-10-28 23:00:53 -0400

    realized that my github has not been pushing, so first real commit in ages

--- scripts/homework1.py
+++ scripts/homework1.py
@@ -1,55 +1,61 @@
 import os
-#Importing modules, modules contain functions that aren't default in Python
-import sys
-# OS Module - open files, see what's in a folder(directory)
-
-
-
-
-
-
-
-
-
-
-
-
+from itertools import chain
 
 file_dir = "../hw_files/"
-#file_dir stands for file directory, the directory that contains the file of interest
 fasta = os.listdir(file_dir)
 infile = open(file_dir+fasta[1],"r")
-#print(infile)
-#infile_lines = infile.readlines()
-#print(infile_lines)
-counter = 0
-n_counter = 0
-test = 0
+outfile_name = "../revised_sequences.fasta"
+
+dict1={}
+temp_key = ''
+temp_number = 0
+
+print(infile)
 
 for line in infile:
-
+    #print(line)
+    line = line.rstrip()
+#    
     if line.startswith(">"):
-        counter +=1
-        s_line = line.split()
-        genome = s_line[0]
+        temp_key =line
+        dict1[temp_key]= ''
+        temp_number +=1
         
-        for letters in genome:
-            n_genome = letters.find("N")
-            #print(test)
-            if n_genome == -1:
-                pass
-            else:
-                n_counter += 1
-                print(genome) 
-                
-    
-                  
-    
-print("There are " + str(counter) + " sequences")
-print("There are " + str(n_counter) + " sequences that contain the letter N") 
+    else:
+        dict1[temp_key] = dict1[temp_key]+ line 
+        #print(temp_key," : ",dict1[temp_key])
+        
+infile.close()
 
-#
-#for line in infile_lines:
-#    if line.startswith(">"):  
-#          
-#        print(counter)
+dict2 = dict1.copy()
+small_num = 0
+n_num = 0
+
+for item in dict2.keys():
+    if (len(dict2[item])<29700):
+        small_num+=1
+        del(dict1[item])
+    elif((len(dict2[item])>29700) and (dict2[item].find("N") > -1)):
+        n_num +=1
+        del(dict1[item])
+       
+       
+print(small_num)
+print(n_num)
+
+dict1 = {value:key for key, value in dict1.items()}
+
+outfile = open(outfile_name,mode="w")
+for key,value in dict1.items():
+    outfile.write(value + "\n" + key + "\n")
+    
+outfile.close()
+
+print "The original file had ", temp_number, " genome records."
+print "The revised file had ", len(dict1), " unique records."
+
+
+
+
+
+
---
+++ scripts/homework3.py
@@ -0,0 +1,9 @@
+import sys
+import os 
+
+
+file_dir = "../hw_files"
+fasta_name = file_dir+"/spike_sequence.fasta"
+infile = open(fasta_name,mode="r")
+
+print(infile)
